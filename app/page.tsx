4def8476d6c83f677cf5fa2c76dd44b43aa9823a
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-22 10:23:03 +0200

    created main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>HelloPage</h1>
+    <div className="flex flex-col flex-start paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
     </div>
   );
 };
